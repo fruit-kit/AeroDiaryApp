336526903a3aac4310c6cc5be12076ce91f97352
fruit-kit <tr3asur3island@gmail.com>
2024-08-09 10:41:23 +0300

    Add logoImageView with constraints and corner radius

--- AeroDiary/Controllers/HomeViewController.swift
+++ AeroDiary/Controllers/HomeViewController.swift
@@ -8,6 +8,20 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    private let logoImageView: UIImageView = {
+        
+        let imageView = UIImageView()
+        
+        imageView.image = UIImage(named: "logo")
+        
+        imageView.contentMode = .scaleAspectFill
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +31,19 @@ class HomeViewController: UIViewController {
         title = "Aero Diary"
         
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.customRed]
+        
+        view.addSubview(logoImageView)
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            logoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            logoImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            logoImageView.heightAnchor.constraint(equalTo: logoImageView.widthAnchor, multiplier: 0.6)
+        ])
+        
+        logoImageView.layer.cornerRadius = 20
+        
+        logoImageView.layer.masksToBounds = true
         
     }
 
