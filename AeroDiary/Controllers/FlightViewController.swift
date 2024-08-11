7a56d0e62f7d9fea3424b0ca4e649bc7d63f5b36
fruit-kit <tr3asur3island@gmail.com>
2024-08-11 13:52:30 +0300

    Add new text fields, stack view, and date picker functionality to FlightViewController

--- AeroDiary/Controllers/FlightViewController.swift
+++ AeroDiary/Controllers/FlightViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class FlightViewController: UIViewController {
     
@@ -13,7 +14,165 @@ class FlightViewController: UIViewController {
         
         let scrollView = UIScrollView()
         
+        scrollView.backgroundColor = .customBlack
+        
         return scrollView
+        
+    }()
+    
+    let contentView: UIView = {
+        
+        let view = UIView()
+        
+        return view
+        
+    }()
+    
+    let stackView: UIStackView = {
+        
+        let stackView = UIStackView()
+        
+        stackView.axis = .vertical
+        
+        stackView.spacing = 16
+        
+        stackView.alignment = .fill
+        
+        return stackView
+        
+    }()
+    
+    let flightNameTextField: UITextField = {
+        
+        let textField = UITextField()
+        
+        textField.placeholder = "Flight name:"
+        
+        textField.textColor = .white
+        
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.darkGray
+        ]
+        
+        textField.attributedPlaceholder = NSAttributedString(string: textField.placeholder ?? "", attributes: attributes)
+        
+        textField.borderStyle = .roundedRect
+        
+        textField.backgroundColor = .customCellColor
+        
+        return textField
+        
+    }()
+    
+    let routeTextField: UITextField = {
+        
+        let textField = UITextField()
+        
+        textField.placeholder = "Route:"
+        
+        textField.textColor = .white
+        
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.darkGray
+        ]
+        
+        textField.attributedPlaceholder = NSAttributedString(string: textField.placeholder ?? "", attributes: attributes)
+        
+        textField.borderStyle = .roundedRect
+        
+        textField.backgroundColor = .customCellColor
+        
+        return textField
+        
+    }()
+    
+    let aircraftTypeTextField: UITextField = {
+        
+        let textField = UITextField()
+        
+        textField.placeholder = "Aircraft type:"
+        
+        textField.textColor = .white
+        
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.darkGray
+        ]
+        
+        textField.attributedPlaceholder = NSAttributedString(string: textField.placeholder ?? "", attributes: attributes)
+        
+        textField.borderStyle = .roundedRect
+        
+        textField.backgroundColor = .customCellColor
+        
+        return textField
+        
+    }()
+    
+    let dateTimeTextField: UITextField = {
+        
+        let textField = UITextField()
+        
+        textField.placeholder = "Date and time:"
+        
+        textField.textColor = .white
+        
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.darkGray
+        ]
+        
+        textField.attributedPlaceholder = NSAttributedString(string: textField.placeholder ?? "", attributes: attributes)
+        
+        textField.borderStyle = .roundedRect
+        
+        textField.backgroundColor = .customCellColor
+        
+        return textField
+        
+    }()
+    
+    let notesTextField: UITextField = {
+        
+        let textField = UITextField()
+        
+        textField.placeholder = "Note:"
+        
+        textField.textColor = .white
+        
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.darkGray
+        ]
+        
+        textField.attributedPlaceholder = NSAttributedString(string: textField.placeholder ?? "", attributes: attributes)
+        
+        textField.borderStyle = .roundedRect
+        
+        textField.backgroundColor = .customCellColor
+        
+        return textField
+        
+    }()
+    
+    let addFlightButton: UIButton = {
+        
+        let button = UIButton(type: .system)
+        
+        button.setTitle("Add flight", for: .normal)
+        
+        button.backgroundColor = .customGreen
+        
+        button.setTitleColor(.white, for: .normal)
+        
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        button.layer.cornerRadius = 10
+        
+        button.snp.makeConstraints { make in
+            
+            make.height.equalTo(50)
+            
+        }
+        
+        return button
         
     }()
     
@@ -28,11 +187,7 @@ class FlightViewController: UIViewController {
         
         setupConstraints()
         
-    }
-    
-    private func addSubviews() {
-        
-        view.addSubview(scrollView)
+        setupDatePicker()
         
     }
     
@@ -44,9 +199,54 @@ class FlightViewController: UIViewController {
     
     private func setupNavigationBar() {
         
-        title = "Flight"
+        title = "Add Flight"
         
-        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.customRed, .font: UIFont.boldSystemFont(ofSize: 24)]
+        navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.customRed,
+            .font: UIFont.boldSystemFont(ofSize: 24)
+        ]
+        
+    }
+    
+    private func setupDatePicker() {
+        
+        let datePicker = UIDatePicker()
+        
+        datePicker.datePickerMode = .dateAndTime
+        
+        datePicker.preferredDatePickerStyle = .wheels
+        
+        datePicker.locale = Locale(identifier: "en_GB")
+        
+        dateTimeTextField.inputView = datePicker
+        
+        let toolbar = UIToolbar()
+        
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneTapped))
+        
+        toolbar.setItems([doneButton], animated: true)
+        
+        dateTimeTextField.inputAccessoryView = toolbar
+        
+    }
+    
+    @objc private func doneTapped() {
+        
+        if let datePicker = dateTimeTextField.inputView as? UIDatePicker {
+            
+            let dateFormatter = DateFormatter()
+            
+            dateFormatter.dateStyle = .medium
+            
+            dateFormatter.timeStyle = .short
+            
+            dateTimeTextField.text = dateFormatter.string(from: datePicker.date)
+            
+        }
+        
+        dateTimeTextField.resignFirstResponder()
         
     }
     
@@ -57,6 +257,42 @@ class FlightViewController: UIViewController {
             make.edges.equalTo(view.safeAreaLayoutGuide)
             
         }
+        
+        contentView.snp.makeConstraints { make in
+            
+            make.edges.equalTo(scrollView)
+            
+            make.width.equalTo(scrollView)
+            
+        }
+        
+        stackView.snp.makeConstraints { make in
+            
+            make.edges.equalTo(contentView).inset(16)
+            
+        }
+        
+    }
+    
+    private func addSubviews() {
+        
+        view.addSubview(scrollView)
+        
+        scrollView.addSubview(contentView)
+        
+        contentView.addSubview(stackView)
+        
+        stackView.addArrangedSubview(flightNameTextField)
+        
+        stackView.addArrangedSubview(routeTextField)
+        
+        stackView.addArrangedSubview(aircraftTypeTextField)
+        
+        stackView.addArrangedSubview(dateTimeTextField)
+        
+        stackView.addArrangedSubview(notesTextField)
+        
+        stackView.addArrangedSubview(addFlightButton)
         
     }
     
--- AeroDiary/Extensions/UIColor+Extensions.swift
+++ AeroDiary/Extensions/UIColor+Extensions.swift
@@ -15,6 +15,8 @@ extension UIColor {
     
     static let customGray = UIColor(red: 80/255, green: 80/255, blue: 80/255, alpha: 1.0)
     
+    static let customGreen = UIColor(red: 40/255, green: 169/255, blue: 10/255, alpha: 1.0)
+    
     static let customCellColor = UIColor(red: 27/255, green: 28/255, blue: 29/255, alpha: 1.0)
     
 }
