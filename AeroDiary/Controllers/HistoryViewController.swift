b75437e296c695a0623187c9b93a2b633dd60724
fruit-kit <tr3asur3island@gmail.com>
2024-08-12 17:46:29 +0300

    Add sort button functionality with alert

--- AeroDiary/Controllers/HistoryViewController.swift
+++ AeroDiary/Controllers/HistoryViewController.swift
@@ -13,8 +13,14 @@ class HistoryViewController: UIViewController {
     
     private let tableView = UITableView()
     
+    private let sortButton = UIButton()
+    
+    private var flights: [FlightsManager.Flight] = FlightsManager.shared.flights
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        flights = FlightsManager.shared.flights
         
         tableView.reloadData()
         
@@ -28,6 +34,8 @@ class HistoryViewController: UIViewController {
         setupNavigationBar()
         
         setupTableView()
+        
+        setupSortButton()
         
         setupConstraints()
         
@@ -43,11 +51,13 @@ class HistoryViewController: UIViewController {
         
         title = "History"
         
-        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.customRed, .font: UIFont.boldSystemFont(ofSize: 24)]
+        navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.customRed,
+            .font: UIFont.boldSystemFont(ofSize: 24)
+        ]
         
     }
     
-    // MARK: - UI Setup
     private func setupTableView() {
         
         tableView.delegate = self
@@ -64,17 +74,77 @@ class HistoryViewController: UIViewController {
         
     }
     
+    private func setupSortButton() {
+        
+        sortButton.setTitle("Sort", for: .normal)
+        
+        sortButton.backgroundColor = .customRed
+        
+        sortButton.layer.cornerRadius = 5
+        
+        sortButton.addTarget(self, action: #selector(sortFlights), for: .touchUpInside)
+        
+        view.addSubview(sortButton)
+        
+    }
+    
     private func setupConstraints() {
         
         tableView.snp.makeConstraints { make in
             
             make.top.equalTo(view.safeAreaLayoutGuide)
             
-            make.leading.trailing.equalToSuperview().inset(20)
+            make.leading.trailing.equalToSuperview()
             
-            make.bottom.equalTo(view.safeAreaLayoutGuide)
+            make.bottom.equalTo(sortButton.snp.top).offset(-16)
             
         }
+        
+        sortButton.snp.makeConstraints { make in
+            
+            make.centerX.equalToSuperview()
+            
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-16)
+            
+            make.width.equalTo(100)
+            
+            make.height.equalTo(40)
+            
+        }
+        
+    }
+    
+    @objc private func sortFlights() {
+        
+        let alert = UIAlertController(title: "Sort by", message: nil, preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "Date & Time", style: .default, handler: { _ in
+            
+            self.flights.sort {
+                
+                $0.dateTime < $1.dateTime
+                
+            }
+            
+            self.tableView.reloadData()
+            
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Name", style: .default, handler: { _ in
+            
+            self.flights.sort {
+                
+                $0.flightName < $1.flightName
+                
+            }
+            
+            self.tableView.reloadData()
+            
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
         
     }
     
@@ -84,7 +154,7 @@ extension HistoryViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return FlightsManager.shared.flights.count
+        return flights.count
         
     }
     
@@ -128,13 +198,13 @@ extension HistoryViewController: UITableViewDelegate, UITableViewDataSource {
         
         flightNameLabel.textColor = .white
         
-        flightNameLabel.text = FlightsManager.shared.flights[indexPath.row].flightName
+        flightNameLabel.text = flights[indexPath.row].flightName
         
         let routeLabel = UILabel()
         
         routeLabel.textColor = .customGray
         
-        routeLabel.text = FlightsManager.shared.flights[indexPath.row].route
+        routeLabel.text = flights[indexPath.row].route
         
         let stackView = UIStackView(arrangedSubviews: [flightNameLabel, routeLabel])
         
@@ -152,7 +222,7 @@ extension HistoryViewController: UITableViewDelegate, UITableViewDataSource {
             
             make.centerY.equalTo(customView)
             
-            make.trailing.lessThanOrEqualTo(customView.snp.trailing).offset(-40)
+            make.trailing.lessThanOrEqualTo(customView.snp.trailing).offset(-32)
             
         }
         
