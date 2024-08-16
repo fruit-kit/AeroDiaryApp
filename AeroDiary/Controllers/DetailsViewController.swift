cc8cfe31ac058779176bb0bf9514fb1bc79f045b
fruit-kit <tr3asur3island@gmail.com>
2024-08-16 17:03:53 +0300

    Add and configure stackView with constraints

--- AeroDiary/Controllers/DetailsViewController.swift
+++ AeroDiary/Controllers/DetailsViewController.swift
@@ -8,6 +8,20 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
+    
+    let stackView: UIStackView = {
+        
+        let stackView = UIStackView()
+        
+        stackView.axis = .vertical
+        
+        stackView.spacing = 16
+        
+        stackView.backgroundColor = .red
+        
+        return stackView
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +29,16 @@ class DetailsViewController: UIViewController {
         setupMainView()
         
         setupNavigationBar()
+        
+        addSubviews()
+        
+        setupConstraints()
+        
+    }
+    
+    private func addSubviews() {
+        
+        view.addSubview(stackView)
         
     }
     
@@ -30,6 +54,13 @@ class DetailsViewController: UIViewController {
         
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.customRed, .font: UIFont.boldSystemFont(ofSize: 24)]
         
+    }
+    
+    private func setupConstraints() {
+        stackView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+            make.width.equalTo(view.snp.width)
+        }
     }
 
 }
