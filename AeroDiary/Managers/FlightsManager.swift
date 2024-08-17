dedd5e267e91d2d6798a18917461adc6dc557ece
fruit-kit <tr3asur3island@gmail.com>
2024-08-17 18:33:55 +0300

    Add persistent storage for flights using UserDefaults with auto-save and load functionality

--- AeroDiary/Managers/FlightsManager.swift
+++ AeroDiary/Managers/FlightsManager.swift
@@ -11,11 +11,23 @@ class FlightsManager {
     
     static let shared = FlightsManager()
     
-    private init() { }
+    private init() {
+        
+        loadFlights()
+        
+    }
     
-    var flights: [Flight] = []
+    var flights: [Flight] = [] {
+        
+        didSet {
+            
+            saveFlights()
+            
+        }
+        
+    }
     
-    struct Flight {
+    struct Flight: Codable {
         
         let flightNumber: String
         
@@ -26,5 +38,24 @@ class FlightsManager {
         let note: String
         
     }
+    
+    private func saveFlights() {
+        
+            if let encodedData = try? JSONEncoder().encode(flights) {
+                UserDefaults.standard.set(encodedData, forKey: "savedFlights")
+                
+            }
+        
+        }
+    
+    private func loadFlights() {
+        
+            if let savedData = UserDefaults.standard.data(forKey: "savedFlights"),
+               let decodedFlights = try? JSONDecoder().decode([Flight].self, from: savedData) {
+                flights = decodedFlights
+                
+            }
+        
+        }
     
 }
